6b7fded93d02c842e5c0f6d56193c6c6d9e5f075
Suraj <surajjadhav9741@gmail.com>
2020-10-02 12:19:11 +0530

    Add : First name check function

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,21 @@
 #!/bin/bash -x
 echo "Welcome to registration form"
+f=0
+first_Name()
+{
+	pattern="^[A-Z]{1}[a-z]{2,}"
+	if [[ $1 =~ $pattern ]]
+	then
+		echo "Valid Name"
+		f=1
+	else
+		echo "Invalid Name"
+		f=0
+	fi
+}
+while [ $f -eq 0 ]
+do
+	echo "Enter the Name :"
+	read name
+	first_Name $name
+done
