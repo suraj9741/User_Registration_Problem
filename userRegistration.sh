16c8168563f5634f1958355c65bc4491527cbba6
Suraj <surajjadhav9741@gmail.com>
2020-10-02 16:27:27 +0530

    Add : Valid Mail check Function

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,35 +1,44 @@
 ##!/bin/bash -x
 echo "Welcome to registration form"
 f=0
-declare -A inputs
-first_Name()
+check()
 {
 	f=0
-	pattern="^[A-Z]{1}[a-z]{2,}"
+	pattern=$2
 	if [[ $1 =~ $pattern ]]
 	then
-		echo "Valid $2 Name"
+		echo "Valid $3 $4"
 		f=1
 		echo ""
 	else
-		echo "Invalid $2 Name"
+		echo "Invalid $3 $4"
 		f=0
 		echo ""
+		echo "Enter $3 $4 again :"
 	fi
 }
-while [ $f -eq 0 ]
-do
-	echo "Enter the First Name :"
-	read fname
-	first_Name $fname First
-done
+loop()
+{
+	while [ $f -eq 0 ]
+	do
+		read input
+		$1 $input $2 $3 $4
+	done
+}
+pattern="^[A-Z]{1}[a-z]{2,}"
+echo "Enter First Name :"
+loop check $pattern First Name
+fname=$input
 f=0
-while [ $f -eq 0 ]
-do
-	echo "Enter the Last Name :"
-	read lname
-	first_Name $lname Last
-done
+echo "Enter Last Name :"
+loop check $pattern Last Name
+lname=$input
+pattern="^([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]?)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$"
+f=0
+echo "Enter Mail :"
+loop check $pattern Email
+email=$input
 
 echo "First Name : $fname"
 echo "Last Name  : $lname"
+echo "Email      : $email"
