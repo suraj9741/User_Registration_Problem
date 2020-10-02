b2a81ecc4a7c20c8e8bbfcb597d27c4628d112cc
Suraj <surajjadhav9741@gmail.com>
2020-10-02 14:04:48 +0530

    Add : Last Name Check Function

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,21 +1,35 @@
-#!/bin/bash -x
+##!/bin/bash -x
 echo "Welcome to registration form"
 f=0
+declare -A inputs
 first_Name()
 {
+	f=0
 	pattern="^[A-Z]{1}[a-z]{2,}"
 	if [[ $1 =~ $pattern ]]
 	then
-		echo "Valid Name"
+		echo "Valid $2 Name"
 		f=1
+		echo ""
 	else
-		echo "Invalid Name"
+		echo "Invalid $2 Name"
 		f=0
+		echo ""
 	fi
 }
 while [ $f -eq 0 ]
 do
-	echo "Enter the Name :"
-	read name
-	first_Name $name
+	echo "Enter the First Name :"
+	read fname
+	first_Name $fname First
 done
+f=0
+while [ $f -eq 0 ]
+do
+	echo "Enter the Last Name :"
+	read lname
+	first_Name $lname Last
+done
+
+echo "First Name : $fname"
+echo "Last Name  : $lname"
